4dcddc663ea73aada217f830f5d4421469eea136
Bilal Lotfi <56886792+bilalltf@users.noreply.github.com>
2021-08-23 04:39:51 +0100

    chatbot update

---
+++ actions/action_institution.py
@@ -0,0 +1,45 @@
+from logging import NullHandler
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import SlotSet
+from rasa_sdk.executor import CollectingDispatcher
+from sqlite3 import *
+class ActionCourse(Action):
+    def name(self) -> Text:
+        return "action_inst"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        exist = False
+        l = ''
+        inst = tracker.get_slot('inst_type')
+        c = connect("database.sqlite")
+        cur = c.cursor()
+
+        if inst.lower() == 'faculté':
+            req = "select * from institution where type = 'faculté'"
+            l = "facultés"
+        elif inst.lower() == 'école':
+            req = "select * from institution where type = 'école'"
+            l = "écoles"
+        else:
+            req = "select * from institution"
+            l = "établissements"
+        response = """Voilà les {} de l'université \n """.format(l)
+        
+
+        
+        res = cur.execute(req)
+
+        for ligne in res.fetchall():
+            response += """- {}, lien {} \n""".format(ligne[1], ligne[3])
+            
+        dispatcher.utter_message(response)
+
+            
+        c.close()
+        
+
+    
+        return [SlotSet('inst_type',NullHandler)]
--- actions/action_teacher.py
+++ actions/action_teacher.py
@@ -12,7 +12,7 @@ class ActionCourse(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         exist = False
-        l = ''
+        teacher = ''
         subj = tracker.get_slot('subject')
         sect = tracker.get_slot('sector')
         c = connect("database.sqlite")
@@ -20,13 +20,14 @@ class ActionCourse(Action):
         req = "select * from course"
         res = cur.execute(req)
         for ligne in res.fetchall():
-            if (subj.lower() == ligne[1].lower() or subj.lower() == ligne[2].lower()) and (sect.lower() == ligne[3].lower() or sect.lower() == ligne[4].lower()) & ligne[5] != None & ligne[5] != '':
+            if (subj.lower() == ligne[1].lower() or subj.lower() == ligne[2].lower()) and (sect.lower() == ligne[3].lower() or sect.lower() == ligne[4].lower()) and ligne[5] != None and ligne[5] != '':
                exist = True 
                teacher = ligne[5]
         c.close()
-        response = """Le professeur du module {} filière {} est {}""".format(subj, sect, teacher)
+        
 
         if exist:
+            response = """Le professeur du module {} filière {} est {}""".format(subj, sect, teacher)
             dispatcher.utter_message(response)
         else: 
             dispatcher.utter_message(text = "Aucun professeur ne correspondant à ces coordonnées")
